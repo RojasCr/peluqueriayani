e8552563a46768139d32519a55bcc719a7f7401c
Cristian Rojas <cristianrojas781@gmail.com>
2023-04-14 21:38:35 -0300

    controller update

--- src/controllers/controller.views.js
+++ src/controllers/controller.views.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.sendFile(`${process.cwd() + "/src/public/home.html"}`)
+    res.sendFile(`${process.cwd()}/src/public/home.html`)
 });
 
 module.exports = router;
